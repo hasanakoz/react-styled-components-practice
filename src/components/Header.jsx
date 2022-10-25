20031806632d1dc1d26e845205689df6a3696f8c
hasan <hasanakoz1986@gmail.com>
2022-10-25 22:18:16 +0300

    react styled components practice files created

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Button from "./styles/Button.styled";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <Button color="#A62440">Apply Courses</Button>
+      <Button bg="#A62440">Talk to an Advisor</Button>
+    </div>
+  );
+};
+
+export default Header;
